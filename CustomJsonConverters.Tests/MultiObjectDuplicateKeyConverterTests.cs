7f4284329d9fdeaa2cf275e927e93d88b2f1a4f7
adam <adam.toner@insightfultechnology.com>
2024-08-29 19:17:53 +0100

    Added MultiObjectDuplicateKeyConverter

---
+++ CustomJsonConverters.Tests/MultiObjectDuplicateKeyConverterTests.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System.Collections.Generic;
+using CustomJsonConverters;
+
+[TestFixture]
+public class MultiObjectDuplicateKeyConverterTests
+{
+    public class RecipientList
+    {
+        public List<string> recipient { get; set; } = new List<string>();
+    }
+    
+    [Test]
+    public void SingleObjectWithDuplicateKeys_ShouldAggregateValues()
+    {
+        // Arrange
+        string json = @"{ ""recipient"": ""George"", ""recipient"": ""Adam"" }";
+        var settings = new JsonSerializerSettings();
+        settings.Converters.Add(new MultiObjectDuplicateKeyConverter<RecipientList>());
+
+        // Act
+        var result = JsonConvert.DeserializeObject<List<RecipientList>>(json, settings);
+
+        // Assert
+        Assert.AreEqual(1, result.Count);
+        Assert.AreEqual(2, result[0].recipient.Count);
+        Assert.Contains("George", result[0].recipient);
+        Assert.Contains("Adam", result[0].recipient);
+    }
+
+    [Test]
+    public void MultipleObjectsWithDuplicateKeys_ShouldAggregateValuesPerObject()
+    {
+        // Arrange
+        string json = @"{ ""recipient"": ""George"", ""recipient"": ""Adam"" }
+                        { ""recipient"": ""Rob"", ""recipient"": ""Josh"", ""recipient"":""Dean"" }";
+        var settings = new JsonSerializerSettings();
+        settings.Converters.Add(new MultiObjectDuplicateKeyConverter<RecipientList>());
+
+        // Act
+        var result = JsonConvert.DeserializeObject<List<RecipientList>>(json, settings);
+
+        // Assert
+        Assert.AreEqual(2, result.Count);
+        Assert.AreEqual(2, result[0].recipient.Count);
+        Assert.AreEqual(3, result[1].recipient.Count);
+
+        Assert.Contains("George", result[0].recipient);
+        Assert.Contains("Adam", result[0].recipient);
+
+        Assert.Contains("Rob", result[1].recipient);
+        Assert.Contains("Josh", result[1].recipient);
+        Assert.Contains("Dean", result[1].recipient);
+    }
+
+    [Test]
+    public void EmptyJsonString_ShouldReturnNull()
+    {
+        // Arrange
+        string json = @"";
+        var settings = new JsonSerializerSettings();
+        settings.Converters.Add(new MultiObjectDuplicateKeyConverter<RecipientList>());
+
+        // Act
+        var result = JsonConvert.DeserializeObject<List<RecipientList>>(json, settings);
+
+        // Assert
+        Assert.IsNull(result);
+    }
+
+    [Test]
+    public void SingleObjectWithNoDuplicateKeys_ShouldDeserializeCorrectly()
+    {
+        // Arrange
+        string json = @"{ ""recipient"": ""George"" }";
+        var settings = new JsonSerializerSettings();
+        settings.Converters.Add(new MultiObjectDuplicateKeyConverter<RecipientList>());
+
+        // Act
+        var result = JsonConvert.DeserializeObject<List<RecipientList>>(json, settings);
+
+        // Assert
+        Assert.AreEqual(1, result.Count);
+        Assert.AreEqual(1, result[0].recipient.Count);
+        Assert.Contains("George", result[0].recipient);
+    }
+
+    [Test]
+    public void MultipleObjectsWithMixedDuplicateAndUniqueKeys_ShouldDeserializeCorrectly()
+    {
+        // Arrange
+        string json = @"{ ""recipient"": ""George"", ""recipient"": ""Adam"" }
+                        { ""recipient"": ""Rob"" }";
+        var settings = new JsonSerializerSettings();
+        settings.Converters.Add(new MultiObjectDuplicateKeyConverter<RecipientList>());
+
+        // Act
+        var result = JsonConvert.DeserializeObject<List<RecipientList>>(json, settings);
+
+        // Assert
+        Assert.AreEqual(2, result.Count);
+        Assert.AreEqual(2, result[0].recipient.Count);
+        Assert.AreEqual(1, result[1].recipient.Count);
+
+        Assert.Contains("George", result[0].recipient);
+        Assert.Contains("Adam", result[0].recipient);
+        Assert.Contains("Rob", result[1].recipient);
+    }
+}
---
+++ CustomJsonConverters/MultiObjectDuplicateKeyConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace CustomJsonConverters
+{
+    public class MultiObjectDuplicateKeyConverter<T> : JsonConverter where T : class, new()
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(List<T>);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var result = new List<T>();
+            var currentObject = new T();
+            reader.SupportMultipleContent = true; 
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.StartObject)
+                {
+                    result.Add(currentObject);
+                    currentObject = new T();
+                }
+                else if (reader.TokenType == JsonToken.PropertyName)
+                {
+                    var propertyName = (string)reader.Value;
+                    reader.Read();
+                    var value = reader.Value;
+
+                    var property = typeof(T).GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance);
+
+                    if (property != null && property.PropertyType.IsGenericType && 
+                        property.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
+                    {
+                        var listType = property.PropertyType.GetGenericArguments()[0];
+                        var list = property.GetValue(currentObject) as IList;
+
+                        if (list == null)
+                        {
+                            list = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(listType));
+                            property.SetValue(currentObject, list);
+                        }
+
+                        list.Add(Convert.ChangeType(value, listType));
+                    }
+                }
+            }
+            result.Add(currentObject);
+
+            return result;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new InvalidOperationException("This converter is only for reading JSON, not writing.");
+        }
+    }
+}
